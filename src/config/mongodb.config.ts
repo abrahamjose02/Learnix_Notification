194d233c27055c87aee86638dddc9905fd7a6f02
Abraham Jose <abrahamjose002@gmail.com>
2024-07-30 16:46:30 +0530

    .env updated

--- src/config/mongodb.config.ts
+++ src/config/mongodb.config.ts
@@ -5,7 +5,7 @@ import 'dotenv/config';
 
 const connectDB = async()=>{
     try {
-        const uri = `${process.env.MONGOURI}/${process.env.MONGO_DATA}`
+        const uri = `${process.env.MONGO_URI}/${process.env.MONGODB_NAME}`
         const conn = await mongoose.connect(uri)
         console.log(`NotificationDB connected : ${conn.connection.host}`)
     } catch (e:any) {
